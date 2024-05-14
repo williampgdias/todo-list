a4581bf2b56e900c29ad2dacd04b32ca09021511
William Dias <smdwilliam89@gmail.com>
2024-05-14 14:14:47 -0300

    feat(todo_list.html): Create the Todo List page.

--- todos/models.py
+++ todos/models.py
@@ -3,6 +3,6 @@ from django.db import models
 
 class Todo(models.Model):
     title = models.CharField(max_length=100, null=False, blank=False)
-    created_at = models.DateTimeField(auto_now_add=True, null=False, blank=False)
-    deadline = models.DateTimeField(null=False, blank=False)
-    finished_at = models.DateTimeField(null=True)
+    created_at = models.DateField(auto_now_add=True, null=False, blank=False)
+    deadline = models.DateField(null=False, blank=False)
+    finished_at = models.DateField(null=True)
