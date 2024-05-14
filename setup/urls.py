1de0d701fdb11e8186472dda8689007cd4a64243
William Dias <smdwilliam89@gmail.com>
2024-05-14 10:36:03 -0300

    feat(todo_list.html): Create the todo_list page and insert the path to it.

--- setup/urls.py
+++ setup/urls.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
 from django.urls import path
 
-from todos.views import home
+from todos.views import todo_list
 
-urlpatterns = [path("admin/", admin.site.urls), path("", home)]
+urlpatterns = [
+    path("admin/", admin.site.urls),
+    path("", todo_list),
+]
